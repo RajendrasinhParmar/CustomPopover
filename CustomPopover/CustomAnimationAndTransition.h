972407120d4a19a87931ad55d6f440f58bbf5f3c
RajendrasinhParmar <rajendra20791@gmail.com>
2015-09-08 00:27:57 +0530

    Add new class files to handle CustomAnimations and transitions

---
+++ CustomPopover/CustomAnimationAndTransition.h
@@ -0,0 +1,13 @@
+//
+//  CustomAnimationAndTransion.h
+//  CustomPopover
+//
+//  Created by Rajendrasinh Parmar on 08/09/15.
+//  Copyright (c) 2015 Rajendrasinh Parmar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomAnimationAndTransion : NSObject
+
+@end
---
+++ CustomPopover/CustomAnimationAndTransition.m
@@ -0,0 +1,13 @@
+//
+//  CustomAnimationAndTransion.m
+//  CustomPopover
+//
+//  Created by Rajendrasinh Parmar on 08/09/15.
+//  Copyright (c) 2015 Rajendrasinh Parmar. All rights reserved.
+//
+
+#import "CustomAnimationAndTransion.h"
+
+@implementation CustomAnimationAndTransion
+
+@end
