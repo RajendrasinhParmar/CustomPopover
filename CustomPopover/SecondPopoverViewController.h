7fedfb0286e47eeaabe874330c1cb3335b66b25d
RajendrasinhParmar <rajendra20791@gmail.com>
2015-09-18 15:46:31 +0530

    Add new file for second popover controller

---
+++ CustomPopover/SecondPopoverViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondPopoverViewController.h
+//  CustomPopover
+//
+//  Created by Rajendrasinh Parmar on 18/09/15.
+//  Copyright (c) 2015 Rajendrasinh Parmar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondPopoverViewController : UIViewController
+
+@end
